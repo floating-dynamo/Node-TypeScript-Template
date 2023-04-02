0b7649622f280fe330550b24e0b38503038f56de
floating-dynamo <shridharmaskeri@gmail.com>
2023-04-02 12:27:00 +0530

    Initial Commit: Setup Template for Node TS

---
+++ README.md
@@ -0,0 +1,15 @@
+# Node TypeScript
+
+## Setting up the Project from scratch
+- npm init -y
+- npm i tsc --save-dev
+- npm i ts-node --save-dev
+- npm i @types/node --save-dev
+- tsc --init
+- npm i express
+- npm i @types/express nodemon --save-dev
+- Inside package.json -> scripts -> "dev": "nodemon src/index.ts"
+
+## Running the template Express Project
+- npm i 
+- npm run dev
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express, {Request, Response} from "express";
+
+const app = express();
+
+const PORT = 8080 || process.env.PORT;
+
+app.use((req: Request, res: Response, next)=>{
+    console.log(req.method, req.path)
+
+    next();
+})
+
+// Parsing Incomming JSON
+app.use(express.json())
+
+app.get("/", (req: Request, res: Response): void=>{
+    res.json({message: "Hello World"})
+})
+
+app.listen(PORT, (): void=>{
+   console.log(`Server started at PORT: ${PORT}`)
+})
